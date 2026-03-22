6309c912c9392b341d9c19c64dec07c615f7d118
Jay Kumar <jay.k.992000@gmail.com>
2026-03-22 15:59:38 +0530

    updated server.js, collections.js, lotus.js

--- src/Products/biotique.js
+++ src/Products/biotique.js
@@ -21,7 +21,7 @@ const query = new URLSearchParams(location.search).get("search");
 
 useEffect(() => {
 axios
-.get("http://localhost:3001/fetchbiotique")
+.get("https://omega-zg6z.onrender.com/fetchbiotique")
 .then((response) => {
 console.log("Fetched Mangoes Pickles products:", response.data); 
 setAllProducts(response.data); 
@@ -38,7 +38,7 @@ useEffect(() => {
 
 if (query) {
 axios
-.get("http://localhost:3001/fetchbiotique", {
+.get("https://omega-zg6z.onrender.com/fetchbiotique", {
 params: { search: query },
 })
 .then((response) => {
